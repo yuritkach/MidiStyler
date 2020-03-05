77a3213ed93fbf46941f9b69b4f8265baa66eb98
Yuri Tkach <yuritkach@gmail.com>
2020-03-05 14:23:15 +0200

    ttt

--- MidiAranger/MidiAranger.Android/MainActivity.cs
+++ MidiAranger/MidiAranger.Android/MainActivity.cs
@@ -6,6 +6,8 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Util;
+using midi;
 
 namespace MidiAranger.Droid
 {
@@ -20,6 +22,33 @@ namespace MidiAranger.Droid
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            MIDISession.GetInstance().Init(this);
+
+            Bundle rinfo = new Bundle();
+            rinfo.PutString(MIDIConstants.RINFO_ADDR, "192.168.2.229");
+            rinfo.PutInt(MIDIConstants.RINFO_PORT, 5008);
+            rinfo.PutBoolean(MIDIConstants.RINFO_RECON, true);
+            MIDISession.GetInstance().Connect(rinfo);
         }
+
+
+        public void sendTestMIDI()
+        {
+            Log.Debug("Main", "sendTestMidi 41,127");
+            Bundle testMessage = new Bundle();
+            testMessage.PutInt(MIDIConstants.MSG_COMMAND, 0x09);
+            testMessage.PutInt(MIDIConstants.MSG_CHANNEL, 0);
+            testMessage.PutInt(MIDIConstants.MSG_NOTE, 41);
+            testMessage.PutInt(MIDIConstants.MSG_VELOCITY, 127);
+            //        MIDIMessage m = MIDIMessage.newUsing(testMessage);
+            MIDISession.GetInstance().SendMessage(testMessage);
+            //        MIDISession.getInstance().sendMessage(41,127);
+            //        MIDIMessage message = MIDISession.getInstance().sendNote(41,127);
+            //        if(message != null) {
+            //            MIDISession.getInstance().sendNote(41,127);
+            //        }
+        }
+
     }
 }
